f05c3a56118891b4971228b6140304a1c847ee90
chetan fasale <fasalechetan1997@gmail.com>
2021-04-16 10:22:20 +0530

    courseform changes

--- src/views/course/CourseRegistration/CourseForm.js
+++ src/views/course/CourseRegistration/CourseForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   CButton,
   CCard,
@@ -29,103 +29,133 @@ import {
   CLabel,
   CSelect,
   CRow,
-  CSwitch
-} from '@coreui/react'
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import CIcon from '@coreui/icons-react'
-import { DocsLink } from 'src/reusable'
+  CSwitch,
+} from "@coreui/react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import CIcon from "@coreui/icons-react";
+import { DocsLink } from "src/reusable";
 
 const CourseForm = () => {
-  const [collapsed, setCollapsed] = React.useState(true)
-  const [showElements, setShowElements] = React.useState(true)
+  const [collapsed, setCollapsed] = React.useState(true);
+  const [showElements, setShowElements] = React.useState(true);
+  const [fields, setFields] = React.useState([{ value: null }]);
+
+  function handleChange(i, event) {
+    const values = [...fields];
+    console.log(values);
+    // values[i].value = event.target.value;
+    // setFields(values);
+  }
+
+  function handleAdd() {
+    const values = [...fields];
+    values.push({ value: null });
+    setFields(values);
+  }
+
+  function handleRemove(i) {
+    const values = [...fields];
+    values.splice(i, 1);
+    setFields(values);
+  }
 
   const formik = useFormik({
     initialValues: {
-      Name: '',
-      Description: '',
-      Duration: '',
-      Fees: '',
-      Syllabus: '',
-      AgeGroupType: '',
-      EnquiryCounter: '',
-      IsActive: '',
-      CoverPhoto: ''
+      Name: "",
+      Description: "",
+      Duration: "",
+      Fees: "",
+      Syllabus: [{topic:'',topic_desc:''}],
+      AgeGroupType: "",
+      EnquiryCounter: "",
+      IsActive: "",
+      CoverPhoto: ""
     },
     validationSchema: yup.object({
-   
-     Name: yup.string()
-     .min(3, 'Name should be greater than 3 characters')
-       
-  
-       .required('Please Enter Course Name'),
-    
-    
-    Description: yup.string()
-     .min(2, 'Description should be greater than 2 characters')
-       
-       .required('Please Enter Course Description'),
-  
-  
-  
-     Duration: yup.string()
-     
-         .required('Please Enter Duration in hours '),
-    
-  
-     Fees: yup.string()
-  
+      Name: yup
+        .string()
+        .min(3, "Name should be greater than 3 characters")
+
+        .required("Please Enter Course Name"),
+
+        
+      Description: yup
+        .string()
+        .min(2, "Description should be greater than 2 characters")
+
+        .required("Please Enter Course Description"),
+
+      Duration: yup
+        .string()
+
+        .required("Please Enter Duration in hours "),
+
+      Fees: yup
+        .string()
+
         //.max(10, 'Mobile number should be 10 digits')
-  
-       .required('Please Enter Fees in rupees'),
-  
-       Syllabus: yup.string()
-  
+
+        .required("Please Enter Fees in rupees"),
+
+      Syllabus: yup
+        .string()
+
         //.max(10, 'Mobile number should be 10 digits')
-  
-       .required('Please Enter Syllabus'),
-       AgeGroupType: yup.string()
-  
+
+        .required("Please Enter Syllabus"),
+      AgeGroupType: yup
+        .string()
+
         //.max(10, 'Mobile number should be 10 digits')
-  
-       .required('Please Enter Age group')
-   }),
-    onSubmit: values => {
-     console.log(values);
-      let demo=JSON.stringify(values);
+
+        .required("Please Enter Age group"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      let demo = JSON.stringify(values);
       console.log(JSON.parse(demo));
-        console.log(demo);
+      console.log(demo);
       fetch("http://localhost:5050/Course/addcourse", {
-          method: 'POST',
-          headers: { 'Content-type': 'application/json' },
-          body: demo
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: demo,
       }).then(() => {
-          console.log("Course added");
+        console.log("Course added");
       });
     },
-  }); 
-  
+  });
 
   return (
     <>
-     <CRow>
-     <CCol >
+      <CRow>
+        <CCol>
           <CCard>
-          <CForm action="" method="post" encType="multipart/form-data" className="form-horizontal" onSubmit={formik.handleSubmit}>
-            <CCardHeader>
-                Course Registration              
-            </CCardHeader>
-    
-            <CCardBody>
-            
-              
-              <CFormGroup row>
+            <CForm
+              action=""
+              method="post"
+              encType="multipart/form-data"
+              className="form-horizontal"
+              onSubmit={formik.handleSubmit}
+            >
+              <CCardHeader>Course Registration</CCardHeader>
+
+              <CCardBody>
+                <CFormGroup row>
                   <CCol md="3">
                     <CLabel htmlFor="text-input">Course Name</CLabel>
                   </CCol>
                   <CCol xs="6" md="6">
-                    <CInput id="text-input" name="Name" placeholder="Text"  value={formik.values.Name} {...formik.getFieldProps("Name")} />
-                    {formik.touched.Name && formik.errors.Name ? <span style={{color:'red'}}>{formik.errors.Name}</span> : null}
+                    <CInput
+                      id="text-input"
+                      name="Name"
+                      placeholder="Text"
+                      value={formik.values.Name}
+                      {...formik.getFieldProps("Name")}
+                    />
+                    {formik.touched.Name && formik.errors.Name ? (
+                      <span style={{ color: "red" }}>{formik.errors.Name}</span>
+                    ) : null}
                     <CFormText>Enter Course Name</CFormText>
                   </CCol>
                 </CFormGroup>
@@ -134,34 +164,57 @@ const CourseForm = () => {
                     <CLabel htmlFor="textarea-input">Course Description</CLabel>
                   </CCol>
                   <CCol xs="6" md="6">
-                    <CTextarea 
-                      name="Description" 
-                      id="textarea-input" 
+                    <CTextarea
+                      name="Description"
+                      id="textarea-input"
                       rows="3"
-                      placeholder="Course Description" 
-                      value={formik.values.Description} {...formik.getFieldProps("Description")}
+                      placeholder="Course Description"
+                      value={formik.values.Description}
+                      {...formik.getFieldProps("Description")}
                     />
-                    {formik.touched.Description && formik.errors.Description ? <span style={{color:'red'}}>{formik.errors.Description}</span> : null}
+                    {formik.touched.Description && formik.errors.Description ? (
+                      <span style={{ color: "red" }}>
+                        {formik.errors.Description}
+                      </span>
+                    ) : null}
                   </CCol>
                 </CFormGroup>
-                
+
                 <CFormGroup row>
                   <CCol md="3">
                     <CLabel htmlFor="text-input">Course Duration</CLabel>
                   </CCol>
                   <CCol xs="6" md="6">
-                    <CInput id="text-input" name="Duration" placeholder="Course Duration" value={formik.values.Duration} {...formik.getFieldProps("Duration")}/>
-                    {formik.touched.Duration && formik.errors.Duration ? <span style={{color:'red'}}>{formik.errors.Duration}</span> : null}
+                    <CInput
+                      id="text-input"
+                      name="Duration"
+                      placeholder="Course Duration"
+                      value={formik.values.Duration}
+                      {...formik.getFieldProps("Duration")}
+                    />
+                    {formik.touched.Duration && formik.errors.Duration ? (
+                      <span style={{ color: "red" }}>
+                        {formik.errors.Duration}
+                      </span>
+                    ) : null}
                     <CFormText>Enter Course Duration</CFormText>
                   </CCol>
-                </CFormGroup>    
+                </CFormGroup>
                 <CFormGroup row>
                   <CCol md="3">
                     <CLabel htmlFor="text-input">Course Fees</CLabel>
                   </CCol>
                   <CCol xs="6" md="6">
-                    <CInput id="text-input" name="Fees" placeholder="Course fees" value={formik.values.Fees} {...formik.getFieldProps("Fees")} />
-                    {formik.touched.Fees && formik.errors.Fees ? <span style={{color:'red'}}>{formik.errors.Fees}</span> : null}
+                    <CInput
+                      id="text-input"
+                      name="Fees"
+                      placeholder="Course fees"
+                      value={formik.values.Fees}
+                      {...formik.getFieldProps("Fees")}
+                    />
+                    {formik.touched.Fees && formik.errors.Fees ? (
+                      <span style={{ color: "red" }}>{formik.errors.Fees}</span>
+                    ) : null}
                     <CFormText>Enter Course Fees</CFormText>
                   </CCol>
                 </CFormGroup>
@@ -170,64 +223,144 @@ const CourseForm = () => {
                     <CLabel htmlFor="textarea-input">Course Syllabus</CLabel>
                   </CCol>
                   <CCol xs="6" md="6">
-                    <CTextarea 
+                    {fields.map((field, idx) => {
+                      return (
+                        <div key={`${field}-${idx}`}>
+                        <CInput
+                      id="topic"
+                      name="topic"
+                      placeholder="Enter topic"
+                      value={formik.values.Syllabus[0].topic}
+                      {...formik.getFieldProps("Syllabus")}
+                    />
+                          <CTextarea
+                            name="topic_desc"
+                            id="topic_desc"
+                            rows="3"
+                            placeholder="Enter topic description"
+                            value={formik.values.Syllabus[0].topic_desc}
+                            {...formik.getFieldProps("Syllabus")}
+                            // onChange={(e) => handleChange(idx, e)}
+                          />
+                          <button
+                            type="button"
+                            onClick={() => handleRemove(idx)}
+                          >
+                            X
+                          </button>
+                        </div>
+                      );
+                    })}
+
+                    {/*<CTextarea 
                       name="textarea-input" 
                       id="textarea-input" 
                       rows="3"
                       placeholder="Course Syllabus" 
                       value={formik.values.Syllabus} {...formik.getFieldProps("Syllabus")}
-                    />
+                    />*/}
+
+                    <button type="button" onClick={() => handleAdd()}>
+                      +
+                    </button>
                   </CCol>
-                  {formik.touched.Syllabus && formik.errors.Syllabus ? <span style={{color:'red'}}>{formik.errors.Syllabus}</span> : null}
+                  {formik.touched.Syllabus && formik.errors.Syllabus ? (
+                    <span style={{ color: "red" }}>
+                      {formik.errors.Syllabus}
+                    </span>
+                  ) : null}
                 </CFormGroup>
                 <CFormGroup row>
-                  <CCol md="3"><CLabel>Eligible AgeGroup</CLabel></CCol>
+                  <CCol md="3">
+                    <CLabel>Eligible AgeGroup</CLabel>
+                  </CCol>
                   <CCol md="9">
                     <CFormGroup variant="checkbox" className="checkbox">
-                      <CInputCheckbox 
-                        id="checkbox1" 
-                        name="checkbox1" 
-                        value="option1" 
+                      <CInputCheckbox
+                        id="checkbox1"
+                        name="checkbox1"
+                        value="option1"
                       />
-                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox1">Option 1</CLabel>
+                      <CLabel
+                        variant="checkbox"
+                        className="form-check-label"
+                        htmlFor="checkbox1"
+                      >
+                        Option 1
+                      </CLabel>
                     </CFormGroup>
                     <CFormGroup variant="checkbox" className="checkbox">
-                      <CInputCheckbox id="checkbox2" name="checkbox2" value="option2" />
-                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox2">Option 2</CLabel>
+                      <CInputCheckbox
+                        id="checkbox2"
+                        name="checkbox2"
+                        value="option2"
+                      />
+                      <CLabel
+                        variant="checkbox"
+                        className="form-check-label"
+                        htmlFor="checkbox2"
+                      >
+                        Option 2
+                      </CLabel>
                     </CFormGroup>
                     <CFormGroup variant="checkbox" className="checkbox">
-                      <CInputCheckbox id="checkbox3" name="checkbox3" value="option3" />
-                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox3">Option 3</CLabel>
+                      <CInputCheckbox
+                        id="checkbox3"
+                        name="checkbox3"
+                        value="option3"
+                      />
+                      <CLabel
+                        variant="checkbox"
+                        className="form-check-label"
+                        htmlFor="checkbox3"
+                      >
+                        Option 3
+                      </CLabel>
                     </CFormGroup>
-                  </CCol>
-                </CFormGroup>
-                
-               
-                <CFormGroup row>
-                  <CLabel col md="3" htmlFor="file-input">Upload Cover Photo</CLabel>
-                  <CCol xs="6" md="6">
-                    <CInputFile id="file-input" name="CoverPhoto" value={formik.values.CoverPhoto} {...formik.getFieldProps("CoverPhoto")}/>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CLabel col md="3" htmlFor="file-input">Upload Video</CLabel>
-                  <CCol xs="6" md="6">
-                    <CInputFile id="file-input" name="file-input"/>
                   </CCol>
                 </CFormGroup>
 
-              
-            </CCardBody>  
-            <CCardFooter>
-              <CButton type="submit" size="sm" color="primary"className="ml-1"> Submit</CButton>
-              <CButton type="reset" size="sm" color="danger"className="ml-1">Reset</CButton>
-            </CCardFooter>
-            </CForm>  
-            </CCard>
-      </CCol>      
+                <CFormGroup row>
+                  <CLabel col md="3" htmlFor="file-input">
+                    Upload Cover Photo
+                  </CLabel>
+                  <CCol xs="6" md="6">
+                    <CInputFile
+                      id="file-input"
+                      name="CoverPhoto"
+                      value={formik.values.CoverPhoto}
+                      {...formik.getFieldProps("CoverPhoto")}
+                    />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                  <CLabel col md="3" htmlFor="file-input">
+                    Upload Video
+                  </CLabel>
+                  <CCol xs="6" md="6">
+                    <CInputFile id="file-input" name="file-input" />
+                  </CCol>
+                </CFormGroup>
+              </CCardBody>
+              <CCardFooter>
+                <CButton
+                  type="submit"
+                  size="sm"
+                  color="primary"
+                  className="ml-1"
+                >
+                  {" "}
+                  Submit
+                </CButton>
+                <CButton type="reset" size="sm" color="danger" className="ml-1">
+                  Reset
+                </CButton>
+              </CCardFooter>
+            </CForm>
+          </CCard>
+        </CCol>
       </CRow>
-      
     </>
-  )
-}
-export default CourseForm
+  );
+};
+export default CourseForm;
